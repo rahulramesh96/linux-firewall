cb26d897657387df6bb51392128b9d07a20b4bbb
rahulramesh96 <cerebralblade@gmail.com>
2022-06-20 16:30:16 -0400

    Land attack

--- firewall.py
+++ firewall.py
@@ -1,15 +1,30 @@
 import os
 from scapy.all import *
+import sys
+
 
 print('Welcome to Linux Firewall')
 
 interface = conf.iface 
-print(interface)
+print()
+print("The working network interface is " + interface)
+ip = get_if_addr(interface)
+print('The IP address: '+ip)
 
 def help():
     print("What attack do you want to stop?")
-    print("Enter 1 to set rules for LAND Attack)
-
+    print("Enter 1 to set rules for LAND Attack")
 
 def landAttack():
-    os.system("iptables -F ; iptables -t nat -F ; iptables -t mangle -F ; iptables -X")
+    os.system("sudo iptables -F ; iptables -t nat -F ; iptables -t mangle -F ; iptables -X")
+    os.system('sudo iptables -A INPUT -s '+  ip + '/24'  + ' j DROP')
+    print('SAFE FROM LAND ATTACK.')
+
+if (len(sys.argv)) == 1:
+    choice = int(input("Enter the choice to stop packet attacks "))
+    if choice == 1:
+        landAttack()
+
+
+
+
